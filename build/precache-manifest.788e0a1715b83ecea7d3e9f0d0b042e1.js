256e7123adbda54a68625461cb70d93fd5864a90
Ammarhr <hariry49@gmail.com>
2020-07-11 21:38:31 +0300

    gh-pages

---
+++ build/precache-manifest.788e0a1715b83ecea7d3e9f0d0b042e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12037df946531b7c28e15e0cb7754d9d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "041c08c9f444f60a2b65",
+    "url": "/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "60f1a9300847c0b3f419",
+    "url": "/todo/static/css/main.22978849.chunk.css"
+  },
+  {
+    "revision": "041c08c9f444f60a2b65",
+    "url": "/todo/static/js/2.b490839a.chunk.js"
+  },
+  {
+    "revision": "d3286c608b7b6d68fb79cf25ce73f2fb",
+    "url": "/todo/static/js/2.b490839a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f1a9300847c0b3f419",
+    "url": "/todo/static/js/main.97fec0d9.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
